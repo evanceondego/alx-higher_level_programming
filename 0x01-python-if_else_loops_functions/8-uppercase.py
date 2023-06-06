4ffb5cd01d51dc9374ad8ed5fa11e675a17cf293
evanceondego <ondegoevance@gmail.com>
2023-06-06 20:42:47 +0000

    8-upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
-se(str):
-    for i in range(len(str)):
-        uni_code = ord(str[i])
-        if uni_code >= 97 and uni_code <= 122:
-            uni_code = uni_code - 32
-        print("{}".format(chr(uni_code)), end='')
-    print()
+#!/usr/bin/python3
+def uppercase(str):
+    i = 0
+    newstr = ""
+    while i != len(str):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            newstr += chr(ord(str[i]) - 32)
+        else:
+            newstr += str[i]
+        i += 1
+    print("{}".format(newstr))
