07049b95fafb59d10f168386f7ee9db3c0e6d4fc
evanceondego <ondegoevance@gmail.com>
2023-06-06 21:46:40 +0000

    101remove

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+har_at(str, n):
+    i = 0
+    newstr = ""
+    while i != len(str):
+        if i == n:
+            pass
+        else:
+            newstr += str[i]
+        i += 1
+    return newstr
