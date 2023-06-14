9ec283b46b12866f0b44a961ee9ef7e02b0f239d
evanceondego <ondegoevance@gmail.com>
2023-06-14 19:19:31 +0000

    6printsorted

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    if(a_dictionary):
+        for x, val in sorted(a_dictionary.items()):
+            print("{}: {}".format(x, val))
