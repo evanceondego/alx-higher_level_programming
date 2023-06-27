22bb13e8577b244810f8da0093424a21657a5b3b
evanceondego <ondegoevance@gmail.com>
2023-06-27 09:23:17 +0000

    updatedprintlistint

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     """
-    Print integers from a list using "{:d}".format().
-    
+    Print the first x integers from a list, skipping non-integer values.
+
     Args:
         my_list (list): The list of values.
         x (int): The number of elements to print from the list.
-        
+
     Returns:
         The count of successfully printed integers.
     """
     count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            continue
+    try:
+        for i in range(x):
+            try:
+                value = my_list[i]
+                if isinstance(value, int):
+                    print("{:d}".format(value), end="")
+                    count += 1
+            except (ValueError, TypeError):
+                continue
+    except IndexError:
+        pass
+    
     print("")
     return count
