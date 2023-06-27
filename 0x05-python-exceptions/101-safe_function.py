0f997c7d994fd77615e1210fe90e14030b6b025c
evanceondego <ondegoevance@gmail.com>
2023-06-27 10:31:36 +0000

    101safefuct

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 
 def safe_function(fct, *args):
@@ -6,5 +7,5 @@ def safe_function(fct, *args):
         result = fct(*args)
         return result
     except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
